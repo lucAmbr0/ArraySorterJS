2559a10633ed2037ef85b31a358d22db55507eef
Luca <luca.ambrosonee@gmail.com>
2024-05-24 20:29:15 +0200

    Uncommented meta theme-color, icon, and manifest links in index.html

---
+++ index.js
@@ -0,0 +1,23 @@
+/* 
+    Array Sorter - JavaScript Functions
+    Made with <3 by @lucAmbr0 
+*/
+
+// ---------------  SERVICE WORKER  ---------------
+
+serviceWorker();
+function serviceWorker() {
+  if ('serviceWorker' in navigator) {
+    console.log('Service worker compatible');
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('https://lucambr0.github.io/ArraySorterJS/service_worker.js')
+        .then(reg => {
+          console.log('Service worker registered');
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    })
+  }
+}
